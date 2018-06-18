3bbfbdefa4b3dcd48f96b6d0bdb7872b9a307fc5
Brian Ward <bward@redhat.com>
2018-06-18 14:19:10 -0400

    Add disable http2.

--- php-pre-start/remove-mod-http2.sh
+++ php-pre-start/remove-mod-http2.sh
@@ -1,3 +1,4 @@
-# comment out or remove this line
+# This does not work with s2i process because it requires root
+# but you could implement this in a secondary build
 # /etc/httpd/conf.modules.d/00-base.conf:LoadModule http2_module modules/mod_http2.so
 sed -e '/mod_http2/ s/^#*/#/' -i /etc/httpd/conf.modules.d/00-base.conf
